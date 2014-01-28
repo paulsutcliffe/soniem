747e251de5fb2229b76059a0610e4b50b599044f
adrielpinzas <adriel@kosmyka.com>
2014-01-28 15:41:36 -0500

    Test de administrador

---
+++ features/admins.feature
@@ -0,0 +1,18 @@
+#language: es
+
+Característica: administración de los contenidos de las web
+
+Escenario: Ingresar como administrador
+  Dado que existe un administrador
+  Y que estoy en la página inicial
+  Cuando visito "/cms/ingresar"
+  Y lleno el formulario con los datos del administrador
+  Cuando presiono "Ingresar"
+  Entonces debería ver "Has iniciado sesión."
+
+Escenario: Cerrar sesión
+  Dado que he iniciado sesión como administrador
+  Y que estoy en la página inicial
+  Cuando hago click en "Cerrar Sesión"
+  Entonces debería ver "Has cerrado la sesión."
+
---
+++ features/step_definitions/admins_steps.rb
@@ -0,0 +1,39 @@
+#Coding: utf-8
+email = 'email@domain.com'
+password = '12345678'
+Dado(/^que existe un administrador$/) do
+  Admin.new(email: email, password: password, password_confirmation: password).save!
+end
+
+Dado(/^que estoy en la página inicial$/) do
+  visit('/inicio/index')
+end
+
+Cuando(/^visito "(.*?)"$/) do |pagina|
+  visit(pagina)
+end
+
+Cuando(/^lleno el formulario con los datos del administrador$/) do
+  fill_in "Email", with: email
+  fill_in "Contraseña", with: password
+end
+
+Cuando(/^presiono "(.*?)"$/) do |boton|
+  click_buttom(botin)
+end
+
+Entonces(/^debería ver "(.*?)"$/) do |resultado|
+  page.should have_content(resultado)
+end
+
+Dado(/^que he iniciado sesión como administrador$/) do
+  Admin.new(:email => email, :password => password, :password_confirmation => password).save!
+  visit('/cms/ingresar')
+  fill_in "Email", :with => email
+  fill_in "Contraseña", :with => password
+  click_button('Ingresar')
+end
+
+Cuando(/^hago click en "(.*?)"$/) do |link|
+  click_link(link)
+end
