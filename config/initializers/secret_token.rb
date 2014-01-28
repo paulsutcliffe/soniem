ad7c1a8877efea4805377eb711b4b9660e9eb8c6
adrielpinzas <adriel@kosmyka.com>
2014-01-28 15:02:01 -0500

    Creada la aplicacion

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Soniem::Application.config.secret_key_base = 'da28dadff9a23d89ed1644b7b78a0701dd49861c670ce5e51930f3d95da658f8e0160956bb2309e73c816b1cc08a658e43b66b3220c06ecc9fcdff48473ec90a'
