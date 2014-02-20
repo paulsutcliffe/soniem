483b9996d362240364f028f97a5e471d07dc2486
Paul Sutcliffe <paul@kosmyka.com>
2014-02-20 17:38:03 -0500

    modificado el mapa

--- app/assets/javascripts/ubicaciones.js
+++ app/assets/javascripts/ubicaciones.js
@@ -2,7 +2,7 @@
     var gm_init;
     gm_init = function() {
     var gm_center, gm_map_type, map_options;
-    gm_center = new google.maps.LatLng(-12.085317,-76.89065);
+    gm_center = new google.maps.LatLng(-12.077491,-77.100635);
     gm_map_type = google.maps.MapTypeId.ROADMAP;
     map_options = {
       center: gm_center,
@@ -18,7 +18,7 @@
     return marker = new google.maps.Marker({
       map: map,
     animation: google.maps.Animation.DROP,
-    position: new google.maps.LatLng(-12.085317,-76.89065),
+    position: new google.maps.LatLng(-12.077491,-77.100635),
     icon: '/assets/gmaps-marker-186a761a2c94865c8c05a60334b8a106.png'
     });
     });
