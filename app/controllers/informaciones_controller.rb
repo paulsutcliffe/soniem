b6d0fca4a0623fa29b35fa4f57164e68b36273dd
adrielpinzas <adriel@kosmyka.com>
2014-03-11 15:35:22 -0500

    incrustado el directivo en quienes somos

--- app/controllers/informaciones_controller.rb
+++ app/controllers/informaciones_controller.rb
@@ -4,8 +4,9 @@ class InformacionesController < InheritedResources::Base
   end
 
   def index
-    @informaciones = Informacion.all
+    @informaciones = Informacion.order("titulo DESC")
     @informacion = params[:informacion_id] ? Informacion.find(params[:informacion_id]) : Informacion.first
+    @directivos = Directivo.all
   end
   
   def show
@@ -15,6 +16,15 @@ class InformacionesController < InheritedResources::Base
       format.js {}
 
       format.json { render json: @informacion }
+    end
+  end
+  def directivos
+    @directivos = Directivo.all
+    respond_to do |format|
+      #format.html
+      format.js {}
+
+      format.json { render json: @directivos }
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Soniem::Application.routes.draw do
 
   resources :formatos
 
+  get "informaciones/directivos"
+
   resources :informaciones
 
   resources :fotografias
@@ -30,12 +32,6 @@ Soniem::Application.routes.draw do
   resources :directivos
 
   resources :convenios
-
-  get "quienes_somos/que_es_soniem"
-  get "quienes_somos/como_funciona_soniem"
-  get "quienes_somos/que_hace_soniem"
-  get "quienes_somos/ley_del_artista"
-  get "quienes_somos/organizacion"
 
   devise_for :socios, :path_names => { :sign_in => 'ingresar', :sign_out => 'salir', :password => 'secreto', :confirmation => 'verificacion', :unlock => 'desbloquear', :registration => 'registro', :sign_up => 'inscribirse' }
 
