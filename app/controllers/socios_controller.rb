015ed3b8ef61d7ba0ebd66e14f7c5e504446a610
adrielpinzas <adriel@kosmyka.com>
2014-03-25 19:10:26 -0500

    controlers

--- app/controllers/socios_controller.rb
+++ app/controllers/socios_controller.rb
@@ -2,4 +2,5 @@ class SociosController < InheritedResources::Base
   def permitted_params
     params.permit(:socio => [:nombre, :contenido, :fotografia, :web, :facebook, :twitter, :soundcloud, fotografias_attributes: [:id, :fotografia, :_destroy], eventos_attributes: [:id, :titulo, :lugar, :fecha, :_destroy]])
   end
+  
 end
