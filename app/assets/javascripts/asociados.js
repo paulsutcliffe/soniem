b9a6c85686dda459c3538cc379e6b08f7f201aaa
adrielpinzas <adriel@kosmyka.com>
2014-03-26 13:47:17 -0500

    update merge

--- app/assets/javascripts/asociados.js
+++ app/assets/javascripts/asociados.js
@@ -1,6 +1,8 @@
 var ready;
 ready = function(){
-  $("#scroller").endlessScroll({ width: '100%', height: '100px', steps: -2, speed: 40, mousestop: false });
+  $("#scroller").endlessScroll({ width: '100%', height: '150px', steps: -2, speed: 40, mousestop: false });
+
+  $("#scroller-down").endlessScroll({ width: '100%', height: '150px', steps: 2, speed: 40, mousestop: false });
 }
 
 $(document).ready(ready);
