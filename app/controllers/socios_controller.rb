e52264a35d774b09b8f4af1f128855f5912be0d3
adrielpinzas <adriel@kosmyka.com>
2014-03-26 18:39:56 -0500

    nested en registrar socio

--- app/controllers/socios_controller.rb
+++ app/controllers/socios_controller.rb
@@ -1,6 +1,6 @@
 class SociosController < InheritedResources::Base
   def permitted_params
-    params.permit(:socio => [:nombre, :contenido, :fotografia, :web, :facebook, :twitter, :soundcloud, fotografias_attributes: [:id, :fotografia, :_destroy], eventos_attributes: [:id, :titulo, :lugar, :fecha, :_destroy], temas_attributes: [:id, :titulo, :autor, :_destroy], audios_attributtes: [:id, :titulo, :audio, :_destroy]])
+    params.permit(:socio => [:nombre, :contenido, :fotografia, :web, :facebook, :twitter, :soundcloud, fotografias_attributes: [:id, :fotografia, :_destroy], eventos_attributes: [:id, :titulo, :lugar, :fecha, :_destroy], temas_attributes: [:id, :titulo, :autor, :_destroy], audios_attributes: [:id, :titulo, :audio, :_destroy]])
   end
   
 end
